41a9c1100569132f72ae3e96b3d154b21663eac7
MarioJ42 <mariojoseph3939@gmail.com>
2026-03-12 13:02:55 +0700

    Log in feature
    Log in frontend & backend
    Frontend for client's home menu
    Register for public inactive

--- event_organizer/routes/web.php
+++ event_organizer/routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+})->name('home');
+
+Route::get('/vendor', function () {
+    return view('vendor');
+})->name('vendor');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
